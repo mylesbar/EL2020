ded88174e2b131ae4b20c3c8626767bc9e50ba82
mylesbar <mylesbar@gmail.com>
2020-05-16 19:58:30 -0400

    server updates

--- code/final/flaskServer.py
+++ code/final/flaskServer.py
@@ -8,15 +8,22 @@
 #And is licenses under the MIT Software License
 
 #Import libraries as needed
-from flask import Flask, render_template, jsonify, Response
+from flask import Flask, render_template, jsonify, Response, request
+from flask import *
+from werkzeug.utils import secure_filename
 import sqlite3 as sql
 import json
 import RPi.GPIO as GPIO
 import time
+import os
 
 #Globals
 #Flask
+UPLOAD_FOLDER ='/log'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if not os.path.isdir(UPLOAD_FOLDER):
+	os.mkdir(UPLOAD_FOLDER)
 
 #GPIO LED stuff
 
@@ -31,20 +38,63 @@ def chartData():
 	con = sql.connect('log/dataLog.db')
 	cur = con.cursor()
 	con.row_factory = sql.Row
-	cur.execute("SELECT Timestamp, Temperature FROM dataLog WHERE temp > 0") #renamed variables to match script
+	cur.execute("SELECT Timestamp, Temperature FROM dataLog WHERE Temperature > 0") #renamed variables to match script
 	dataset = cur.fetchall()
 	print (dataset)
 	chartData = []
+
 	for row in dataset:
 		chartData.append({"Date": row[0], "Temperature": float(   row[1][:-2]      )})
+
 	return Response(json.dumps(chartData), mimetype='application/json')
 
+@app.route("/sqlHumidData")
+def humidData():
+	con = sql.connect('log/dataLog.db')
+	cur = con.cursor()
+	con.row_factory = sql.Row
 
-@app.route("/button")
+	cur.execute("SELECT Timestamp, Humidity FROM dataLog WHERE Humidity > 0")
+	humidSet = cur.fetchall()
+	humidityData = []
+	print(humidSet)
+
+	for row in humidSet:
+		humidityData.append({"Date": row[0], "Humidity": float(   row[1][:-2]      )})
+
+	return Response(json.dumps(humidityData), mimetype='application/json')
+
+@app.route("/sqlPressureData")
+def pressureData():
+	con = sql.connect('log/dataLog.db')
+	cur = con.cursor()
+	con.row_factory = sql.Row
+
+	cur.execute("SELECT Timestamp, Pressure FROM dataLog WHere Pressure > 0")
+	pressureSet = cur.fetchall()
+	pressureData = []
+	print(pressureSet)
+
+	for row in pressureSet:
+		pressureData.append({"Date": row[0], "Pressure": float(   row[1][:-2]      )})
+
+	return Response(json.dumps(pressureData), mimetype='application/json')
+
+
+@app.route('/button', methods = ['GET', 'POST'])
+
 def button():
-        #Play Midi File Here
+	if request.method == 'POST':
+		f = request.files['../log/TrashBeat.mid']
+		f.save(secure_filename(f.filename))
+	return 'file uploaded successfuly'
 
-	return Response()
+#send midifile to server for playback
+	filename = 'TrashBeat.mid'
+	print('sending')
+	return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+#	return Response()
 
 
 if __name__ == "__main__":
